4ad8db19a23850e3dbb3f7cd7c9abd01356dbdc9
sharon <lspss95270@gmail.com>
2024-08-16 23:36:37 +0800

    chore: Clean up code 0816

--- GPU_test.py
+++ GPU_test.py
@@ -1,16 +1,4 @@
 
-
-# import torch
-# print(torch.version.cuda)
-
-# print(torch.cuda.is_available())
-# print(torch.cuda.device_count())
-# print(torch.cuda.current_device())
-# print(torch.cuda.get_device_name(0))
-
-# from torch.backends import cudnn
-# print(cudnn.is_available())
-
 import tensorflow as tf
 
 print(tf.__version__)
